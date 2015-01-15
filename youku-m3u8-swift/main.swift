539dbde0dfb896c7fa03ed773df3cf420f7fcfd6
hyx-cn <huyixun@gmail.com>
2015-01-15 18:07:17 +0800

    Initial Commit

---
+++ youku-m3u8-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  youku-m3u8-swift
+//
+//  Created by hyx on 15/1/15.
+//  Copyright (c) 2015年 hyx. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
